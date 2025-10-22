c2eefa4b21aa42336c8b8c266a342227e8aca839
bharathi175 <bharathiarivu17@gmail.com>
2025-10-22 08:59:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var random1=Math.floor(1+Math.random()*6);
-var randomdice1="dice"+random1+".png";//dice q to 6
-var image1="images/"+randomdice1;
-var i1=document.querySelectorAll("img")[0];//0 because index 0 th ku generate pannanum
-i1.setAttribute("src",image1);
-var random2 = Math.floor(1 + Math.random() * 6);
-var randomdice2 = "dice" + random2 + ".png"; //dice q to 6
-var image2 = "images/" + randomdice2;
-var i2= document.querySelectorAll("img")[1]; //0 because index 0 th ku generate pannanum
-i2.setAttribute("src", image2);
-if(random1>random2)
-{
-    document.querySelector("h1").innerHTML="player 1 wins";
-}
-else{
-    document.querySelector("h1").innerHTML="player 2 wins";
+// Player 1
+var random1 = Math.floor(1 + Math.random() * 6);
+var randomdice1 = "dice" + random1 + ".png"; // dice1.png to dice6.png
+var i1 = document.querySelectorAll("img")[0]; // first image
+i1.setAttribute("src", randomdice1); // removed "images/"
 
+// Player 2
+var random2 = Math.floor(1 + Math.random() * 6);
+var randomdice2 = "dice" + random2 + ".png";
+var i2 = document.querySelectorAll("img")[1]; // second image
+i2.setAttribute("src", randomdice2); // removed "images/"
+
+// Winner
+if(random1 > random2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if(random2 > random1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
 }
